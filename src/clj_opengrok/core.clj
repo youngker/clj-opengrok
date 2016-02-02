3e03c836ec677e5631885685778093828e927586
YoungJoo Lee <youngker@gmail.com>
2016-02-02 17:51:37 +0900

    study-1

--- src/clj_opengrok/core.clj
+++ src/clj_opengrok/core.clj
@@ -1,6 +1,8 @@
 (ns clj-opengrok.core
-  (:import (java.io StringReader File ByteArrayInputStream ObjectInputStream)
+  (:import (java.io StringReader File ByteArrayInputStream ObjectInputStream
+                    InputStreamReader FileInputStream)
            (org.opensolaris.opengrok.analysis Definitions Definitions$Tag)
+           (org.opensolaris.opengrok.search QueryBuilder Hit)
            (org.opensolaris.opengrok.search.context Context)
            (org.apache.lucene.analysis Analyzer TokenStream)
            (org.apache.lucene.analysis.standard StandardAnalyzer)
@@ -17,32 +19,78 @@
            (org.apache.lucene.util Version AttributeSource BytesRef)
            (org.apache.lucene.store FSDirectory NIOFSDirectory RAMDirectory Directory)))
 
-(def ^{:dynamic true} *version* Version/LUCENE_CURRENT)
-(def ^{:dynamic true} *analyzer* (StandardAnalyzer. *version*))
+(defn get-query-builder []
+  (QueryBuilder.))
+
+(defn get-query [query-builder query]
+  (.build (.setDefs query-builder query)))
+
+(defn get-context [query query-builder]
+  (Context. query (.getQueries query-builder)))
 
 (defn get-searcher [dir]
-  (IndexSearcher. (DirectoryReader/open (FSDirectory/open (File. dir)))))
+  (IndexSearcher.
+   (DirectoryReader/open
+    (FSDirectory/open (File. dir)))))
 
 (defn get-sort []
   (Sort. (SortField. "date" SortField$Type/STRING true)))
 
-(defn get-docs []
-  (let [parser (QueryParser. *version* "full" *analyzer*)
-        query (.parse parser "google")
-        searcher (get-searcher
-                  "/Users/youngker/Projects/lp/.opengrok/index/frameworks")
-        fdocs (.search searcher query 25 (get-sort))
-        totalhits (.totalHits fdocs)
-        hits (.scoreDocs fdocs)]
-    (map #(.doc searcher (.doc %)) hits)))
+(defn get-fdocs [searcher query]
+  (.search searcher query 5 (get-sort)))
+
+(defn get-total-hits [fdocs]
+  (.totalHits fdocs))
+
+(defn get-hits [fdocs]
+  (.scoreDocs fdocs))
+
+(defn get-docs [searcher hits]
+  (map #(.doc searcher (.doc %)) hits))
+
+(defn get-tags-field [doc]
+  (.getField doc "tags"))
+
+(defn get-taglist [tag]
+  (map #(list (.line %) "///" (.text %) "///" (.symbol %) "///" (.type %)) tag))
 
 (defn get-data [^Definitions definition]
   (let [^Definitions$Tag tag (.getTags definition)]
-    (.type (first tag))))
+    (get-taglist tag)))
 
-(defn get-tags [doc]
-  (when-let [tags-field (.getField doc "tags")]
-    (get-data (Definitions/deserialize (.bytes (.binaryValue tags-field))))))
+(defn get-tag [context doc]
+  (println "get-tag")
+  (println (.get doc "t"))
+  (let [filename (.get doc "path")
+        tag-field (get-tags-field doc)
+        tag (if tag-field
+              (Definitions/deserialize (.bytes (.binaryValue
+                                                tag-field)))
+              nil)
+        hit (list (Hit.))]
+    (.getContext context
+                 (InputStreamReader. (FileInputStream. (str
+                                                        "/path/to"
+                                                        filename))) nil nil nil
+                 filename tag false false hit)
+    (println "line num"(.getLineno (first hit)))
+    ))
 
-(defn get-xxx []
-  (map #(get-tags %) (get-docs)))
+(defn get-tags [context docs]
+  (println "get-tags")
+  (map #(get-tag context %) docs))
+
+(defn search []
+  (let [query-builder (get-query-builder)
+        query (get-query query-builder "test")
+        context (get-context query query-builder)
+        searcher (get-searcher
+                  "/path/to")
+        fdocs (get-fdocs searcher query)
+        totalhits (get-total-hits fdocs)
+        hits (get-hits fdocs)
+        docs (get-docs searcher hits)
+        tags (get-tags context docs)]
+    (println "total hits: " totalhits)
+    ;;    (println tags)
+    ))
