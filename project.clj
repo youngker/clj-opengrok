da75f3b367a92067c598671a1ed34b2e8572250e
YoungJoo Lee <youngker@gmail.com>
2016-02-03 17:11:34 +0900

    study-2

--- project.clj
+++ project.clj
@@ -5,8 +5,12 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [lein-localrepo "0.5.3"]
+                 [org.clojure/tools.cli "0.3.3"]
                  [org.opensolaris/opengrok "0.12.1.5"]
+                 [org.apache.commons/jrcs "0.12.1.5"]
                  [org.apache.lucene/lucene-core "4.7.0"]
                  [org.apache.lucene/lucene-queryparser "4.7.0"]
                  [org.apache.lucene/lucene-analyzers-common "4.7.0"]
-                 [org.apache.lucene/lucene-highlighter "4.7.0"]])
+                 [org.apache.lucene/lucene-highlighter "4.7.0"]]
+  :aot [clj-opengrok.core]
+  :main clj-opengrok.core)
--- src/clj_opengrok/core.clj
+++ src/clj_opengrok/core.clj
@@ -1,9 +1,15 @@
 (ns clj-opengrok.core
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clojure.string :as string])
+  (:gen-class)
   (:import (java.io StringReader File ByteArrayInputStream ObjectInputStream
                     InputStreamReader FileInputStream)
+           (java.util ArrayList)
+           (org.opensolaris.opengrok.history RepositoryFactory)
            (org.opensolaris.opengrok.analysis Definitions Definitions$Tag)
            (org.opensolaris.opengrok.search QueryBuilder Hit)
            (org.opensolaris.opengrok.search.context Context)
+           (org.opensolaris.opengrok.configuration RuntimeEnvironment)
            (org.apache.lucene.analysis Analyzer TokenStream)
            (org.apache.lucene.analysis.standard StandardAnalyzer)
            (org.apache.lucene.document Document Field Field$Index Field$Store)
@@ -18,6 +24,16 @@
                                                SimpleHTMLFormatter)
            (org.apache.lucene.util Version AttributeSource BytesRef)
            (org.apache.lucene.store FSDirectory NIOFSDirectory RAMDirectory Directory)))
+
+(def cli-options
+  [["-R" nil "<configuration.xml> Read configuration from the specified file"]
+   ["-d" nil "Symbol Definitions"]
+   ["-r" nil "Symbol References"]
+   ["-p" nil "Path"]
+   ["-h" nil "History"]
+   ["-f" nil "Full Text"]
+   ["-t" nil "Type"]
+   ["-h" "--help" "Help"]])
 
 (defn get-query-builder []
   (QueryBuilder.))
@@ -37,7 +53,7 @@
   (Sort. (SortField. "date" SortField$Type/STRING true)))
 
 (defn get-fdocs [searcher query]
-  (.search searcher query 5 (get-sort)))
+  (.search searcher query 25 (get-sort)))
 
 (defn get-total-hits [fdocs]
   (.totalHits fdocs))
@@ -58,39 +74,96 @@
   (let [^Definitions$Tag tag (.getTags definition)]
     (get-taglist tag)))
 
+(defn get-root-path []
+  (.getSourceRootPath (RuntimeEnvironment/getInstance)))
+
+(defn get-data-root-path []
+  (.getDataRootFile (RuntimeEnvironment/getInstance)))
+
+(defn print-hit [hit]
+  (let [root (get-root-path)
+        file (File. root (.getPath hit))]
+    (println (str (.getAbsolutePath file) ":" (.getLineno hit) ":") (.getLine hit))))
+
 (defn get-tag [context doc]
-  (println "get-tag")
-  (println (.get doc "t"))
   (let [filename (.get doc "path")
         tag-field (get-tags-field doc)
-        tag (if tag-field
-              (Definitions/deserialize (.bytes (.binaryValue
-                                                tag-field)))
-              nil)
-        hit (list (Hit.))]
+        hit (ArrayList.)
+        tags (if tag-field
+               (Definitions/deserialize (.bytes (.binaryValue
+                                                 tag-field)))
+               nil)]
     (.getContext context
                  (InputStreamReader. (FileInputStream. (str
-                                                        "/path/to"
+                                                        (get-root-path)
                                                         filename))) nil nil nil
-                 filename tag false false hit)
-    (println "line num"(.getLineno (first hit)))
-    ))
+                 filename tags false false hit)
+    (map #(print-hit %) hit)))
 
 (defn get-tags [context docs]
-  (println "get-tags")
   (map #(get-tag context %) docs))
 
+(defn ^:static set-configuration [conf]
+  (try
+    (.readConfiguration (RuntimeEnvironment/getInstance) (File. conf))
+    (catch Exception e (str "Failed to read config file: " (.getMessage e)))))
+
+;;(search)
 (defn search []
-  (let [query-builder (get-query-builder)
-        query (get-query query-builder "test")
+  (let [root (get-root-path)
+        query-builder (get-query-builder)
+        query (get-query query-builder "google")
         context (get-context query query-builder)
         searcher (get-searcher
-                  "/path/to")
+                  (str (get-data-root-path) "/index"))
         fdocs (get-fdocs searcher query)
         totalhits (get-total-hits fdocs)
         hits (get-hits fdocs)
         docs (get-docs searcher hits)
         tags (get-tags context docs)]
     (println "total hits: " totalhits)
-    ;;    (println tags)
-    ))
+    (println "total hits: " (count hits))
+    tags))
+
+(defn usage [options-summary]
+  (->> ["Search -R <configuration.xml> [-d | -r | -p | -h | -f | -t] 'query string' .."
+        ""
+        "Usage: program-name [options] action"
+        ""
+        "Options:"
+        options-summary
+        ""
+        "Actions:"
+        "  start    Start a new server"
+        "  stop     Stop an existing server"
+        "  status   Print a server's status"
+        ""
+        "Please refer to the manual page for more information."]
+       (string/join \newline)))
+
+(defn error-msg [errors]
+  (str "The following errors occurred while parsing your command:\n\n"
+       (string/join \newline errors)))
+
+(defn exit [status msg]
+  (println msg)
+  (System/exit status))
+
+(defn -main [& args]
+  ;; (let [{:keys [options arguments errors summary]} (parse-opts args cli-options)]
+  ;;   (cond
+  ;;     (:help options) (exit 0 (usage summary))
+  ;;     (not= (count arguments) 1) (exit 1 (usage summary))
+  ;;     errors (exit 1 (error-msg errors)))
+
+  ;;   ;; (case (first arguments)
+  ;;   ;;   "start" (server/start! options)
+  ;;   ;;   "stop" (server/stop! options)
+  ;;   ;;   "status" (server/status! options)
+  ;;   ;;   (exit 1 (usage summary)))
+
+  (set-configuration
+   "/path/to")
+  (search)
+  (println "done!")
+  )
